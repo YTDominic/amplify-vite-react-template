fc6fa84f9b8928a4f5eec32a0458dd5d29b1d2de
YTDominic <klncat01@gmail.com>
2024-12-17 17:38:16 +0000

    Update the front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,56 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-
-
 const client = generateClient<Schema>();
+import "./App.css";
 
 function App() {
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
 
+  const sortedTodos = [...todos].sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+
   function createTodo() {
     client.models.Todo.create(
-      { content: window.prompt("Meter Reading"),
-        machineID: window.prompt("Machine Name")
+      {     
+        machineID: window.prompt("Machine Name"),
+        content: window.prompt("Meter Reading")
       });
   }
 
+  const formatToReadableUTC = (isoString: string): string => {
+    const date = new Date(isoString);
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const month = date.toLocaleString('en-US', { month: 'short', timeZone: 'UTC' });
+    const year = date.getUTCFullYear();
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+    
+    return `${day} ${month} ${year}, ${hours}:${minutes} UTC`;
+  };
+  
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.machineID}+{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+    <main>    
+      <h1>Energy Saving</h1>
+      <button onClick={createTodo}>+ new</button> 
+      <div className="container">
+        <h1 className="header">Meter Readings</h1>
+          <div className="grid-container">
+            {sortedTodos.map((todo) => (
+              <div key={todo.id} className="card">
+                <h2 className="card-title">{todo.machineID}</h2>
+                <p className="card-content">{todo.content}</p>
+                <p className="card-date">{formatToReadableUTC(todo.createdAt)}</p>
+              </div>
+          ))}
+          </div>
       </div>
     </main>
   );
